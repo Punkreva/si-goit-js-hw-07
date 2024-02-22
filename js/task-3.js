c8c8dddb2662d48d7b00c968e5ab8e12fdfee07a
Sotula Ihor <sotulaiv@gmail.com>
2024-02-22 13:31:38 +0200

    task-3

---
+++ js/task-3.js
@@ -0,0 +1,8 @@
+const textInput = document.querySelector("#name-input");
+
+const output = document.querySelector("#name-output");
+
+textInput.addEventListener("input", (event) => {
+    let trimmedValue = event.currentTarget.value.trim();
+     output.textContent = trimmedValue.length > 0 ? trimmedValue : "Anonymous";
+ });
